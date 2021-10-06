d495fcf2e5759db3a4380d31b3fdfff9dec3df52
Yifei Simon Shao <syifei@umich.edu>
2021-10-06 16:02:19 -0400

    Compilable Version, waiting for tests on rover Friday

--- src/planner/src/interpolator.cpp
+++ src/planner/src/interpolator.cpp
@@ -1,9 +1,10 @@
 #include <ros/ros.h>
-#include <algorithm.h>
+#include <algorithm>
 #include <tuple>
 #include <geometry_msgs/Pose.h>
 #include <nav_msgs/Path.h>
 #include <geometry_msgs/Point.h>
+#include <tf/transform_listener.h>
 #include <tf/tf.h>
 
 double euc_dist (const geometry_msgs::Point p1, geometry_msgs::Point p2) {
@@ -38,11 +39,10 @@ std::tuple<int, double, double> minimum_dist_on_poly_line (const geometry_msgs::
     }
     int p_idx = std::min_element(agent_dist.begin(),agent_dist.end()) - agent_dist.begin();
     double d0 = cum_dist.at(p_idx);
-    double d1 = euc_dist(path.at(p_idx),state)
+    double d1 = euc_dist(path.at(p_idx),state);
     double d = d0 + d1;
     double d_best = cum_dist.back();
-    auto min_d = std::make_tuple(p_idx,d,d_best);
-    return min_d;
+    return std::make_tuple(p_idx,d,d_best);
 }
 
 double cal_tri_area (geometry_msgs::Point p1, geometry_msgs::Point p2, geometry_msgs::Point p3) {
@@ -50,14 +50,14 @@ double cal_tri_area (geometry_msgs::Point p1, geometry_msgs::Point p2, geometry_
 }
 
 double cal_curvature (geometry_msgs::Point p1, geometry_msgs::Point p2, geometry_msgs::Point p3) {
-    return 4*tri_area(p1,p2,p3)/(euc_dist(p1,p2)*euc_dist(p1,p3)*euc_dist(p2,p3));
+    return 4*cal_tri_area(p1,p2,p3)/(euc_dist(p1,p2)*euc_dist(p1,p3)*euc_dist(p2,p3));
 }
 
 geometry_msgs::Point match_trajectory_xy(double dist, std::vector<double> cum_dist, std::vector<geometry_msgs::Point> path) {
     geometry_msgs::Point result;
     if (dist <= cum_dist.back()) {
         for(int i = 0; i < cum_dist.size(); i++) {
-            if ((dist >= cum_dist.at(i)) && (dist <= cum_dist.at(i+1)) {
+            if ((dist >= cum_dist.at(i)) && (dist <= cum_dist.at(i+1))) {
                 double p = (dist-cum_dist.at(i))/(cum_dist.at(i+1)-cum_dist.at(i));
                 result.x = path.at(i).x + (path.at(i+1).x-path.at(i).x)*p;
                 result.y = path.at(i).y + (path.at(i+1).y-path.at(i).y)*p;
@@ -75,7 +75,7 @@ double match_trajectory_heading(double dist, std::vector<double> cum_dist, std::
     double result;
     if (dist <= cum_dist.back()) {
         for(int i = 0; i < cum_dist.size(); i++) {
-            if ((dist >= cum_dist.at(i)) && (dist <= cum_dist.at(i+1)) {
+            if ((dist >= cum_dist.at(i)) && (dist <= cum_dist.at(i+1))) {
                 double p = (dist-cum_dist.at(i))/(cum_dist.at(i+1)-cum_dist.at(i));
                 result = path.at(i).z + (path.at(i+1).z-path.at(i).z);
                 break;
@@ -87,7 +87,7 @@ double match_trajectory_heading(double dist, std::vector<double> cum_dist, std::
     return result;
 }
 
-std::tuple<geometry_msgs::Point,geometry_msgs::Point,geometry_msgs::Point> get_waypoints (const geometry_msgs::Point state, const std::vector<geometry_msgs::Point> path) {
+std::vector<geometry_msgs::Point> get_waypoints (const geometry_msgs::Point state, const std::vector<geometry_msgs::Point> path) {
     //Constants Definition
     double reduced_curve_lookahead = 7.0;
     double lookahead_distance, heading_lookahead;
@@ -100,15 +100,14 @@ std::tuple<geometry_msgs::Point,geometry_msgs::Point,geometry_msgs::Point> get_w
     int p_idx = std::get<0>(min_d);
     double d = std::get<1>(min_d);
     double d_best = std::get<2>(min_d);
-
     if (p_idx + int(reduced_curve_lookahead*20) < path.size()) {
         for (int i = p_idx; i <= p_idx + int(reduced_curve_lookahead*20); i++) {
-            if (p_idx == 0) {
-                curvatures.push_back(abs(cal_curvature(path.at(p_idx),path.at(p_idx+1),path.at(p_idex+2))));
-            }else if(p_idex == path.size()-1) {
-                curvatures.push_back(abs(cal_curvature(path.at(p_idx),path.at(p_idx-1),path.at(p_idex-2))));
+            if (i == 0) {
+                curvatures.push_back(abs(cal_curvature(path.at(i),path.at(i+1),path.at(i+2))));
+            }else if(i == path.size()-1) {
+                curvatures.push_back(abs(cal_curvature(path.at(i-2),path.at(i-1),path.at(i))));
             }else {
-                curvatures.push_back(abs(cal_curvature(path.at(p_idx-1),path.at(p_idx),path.at(p_idex+1)))); 
+                curvatures.push_back(abs(cal_curvature(path.at(i-1),path.at(i),path.at(i+1)))); 
             }
         }
         double k = *std::min_element(curvatures.begin(),curvatures.end());
@@ -137,11 +136,12 @@ std::tuple<geometry_msgs::Point,geometry_msgs::Point,geometry_msgs::Point> get_w
     d_lkhd2 = std::min(d_best, d + 2*heading_lookahead/3);
     wp2.z = match_trajectory_heading(d_lkhd2, cum_dist, path);
 
-    auto result = std::make_tuple(wp,wp1,wp2);
+    std::vector<geometry_msgs::Point> result{wp,wp1,wp2};
     return result;
 }
 
 double cal_yaw(const tf::Quaternion q) {
+    double roll, pitch, yaw;
     tf::Matrix3x3 m(q);
     m.getRPY(roll, pitch, yaw);
     return yaw; 
@@ -149,48 +149,58 @@ double cal_yaw(const tf::Quaternion q) {
 
 std::vector<geometry_msgs::Point> curr_path;
 geometry_msgs::Point agent_state;
+double path_updated = false;
 
 void path_callback(const nav_msgs::Path path) {
-    int s = sizeof(path.poses)/sizeof(path.poses[0]);
-    curr_path.clear();
+    int s = path.poses.size();
     geometry_msgs::Point way_point;
     double roll, pitch, yaw;
-    for(int i = 0; i < s; i++) {
-        way_point.x = path.poses[i].x;
-        way_point.y = path.poses[i].y;
-        tf::Quaternion q(
-        path.poses[i].orientation.x,
-        path.poses[i].orientation.y,
-        path.poses[i].orientation.z,
-        path.poses[i].orientation.w);
-        way_point.z = cal_yaw(q);
-        curr_path.push_back(way_point);
+    if (s>3) {
+        curr_path.clear();
+    	for(int i = 0; i < s; i++) {
+          way_point.x = path.poses[i].pose.position.x;
+          way_point.y = path.poses[i].pose.position.y;
+          tf::Quaternion q(
+          path.poses[i].pose.orientation.x,
+          path.poses[i].pose.orientation.y,
+          path.poses[i].pose.orientation.z,
+          path.poses[i].pose.orientation.w);
+          way_point.z = cal_yaw(q);
+          curr_path.push_back(way_point);
+        }
+        path_updated = true;
     }
 }
 
 int main(int argc, char** argv){
     ros::init(argc, argv, "waypoint_interpolator");
-    ros::NodeHandle node;
+    ros::NodeHandle nh;
     ros::Subscriber sub1 = nh.subscribe("/move_base/GlobalPlanner/plan", 1, path_callback);
-    ros::Publisher pub1 = nb.advertise<nav_msgs::Path>("/interpolator/way_points",100);
+    ros::Publisher pub1 = nh.advertise<nav_msgs::Path>("/interpolator/way_points",100);
     ros::Rate loop_rate(1);
     tf::TransformListener listener;
-    std::tuple way_points;
+    std::vector<geometry_msgs::Point> way_points;
     nav_msgs::Path wpts;
     while(ros::ok()) {
         tf::StampedTransform transform;
-        listener.waitForTransform("/map", "/base_link", ros::Time(0), ros::Duration(0.1));
+        geometry_msgs::PoseStamped w;
+        listener.waitForTransform("/map", "/base_link", ros::Time(0), ros::Duration(3.0));
         listener.lookupTransform("/map", "/base_link", ros::Time(0), transform);
-        agent_state.x = transform.getOrigin().x();
-        agent_state.y = transform.getOrigin().y();
-        agent_state.z = cal_yaw(transform.getRotation());
-        way_points = get_waypoints(agent_state,curr_path);
-        wpts.header.time = ros::Time::now();
-        wpts.header.frame_id = "map";
-        for(int i = 0; i < 3; i++) {
-            wpts.poses[i].pose.position = std::get<0>(way_points);
+        if (path_updated == true) {
+          agent_state.x = transform.getOrigin().x();
+          agent_state.y = transform.getOrigin().y();
+          agent_state.z = cal_yaw(transform.getRotation());
+          way_points = get_waypoints(agent_state,curr_path);
+          wpts.header.stamp = ros::Time::now();
+          wpts.header.frame_id = "map";
+          for(int i = 0; i < 3; i++) {
+            w.pose.position = way_points.at(i);
+            wpts.poses.push_back(w);
+          }
+          pub1.publish(wpts);
+          wpts.poses.clear();
+          path_updated = false;
         }
-        pub1.publish(wpts);
         ros::spinOnce();
         loop_rate.sleep();
     }
